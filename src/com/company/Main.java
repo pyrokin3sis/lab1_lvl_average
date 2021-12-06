8de35d01de9574e721f99ab0bdad786039368102
Danny Polischuk <polishukdanya12@gmail.com>
2021-12-06 15:59:50 +0200

    lab1_average_lvl

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+public class shop
+{
+    public String naimenuv = new String(new char[20]);
+    public int cina;
+    public float datavir;
+    public int termin;
+    public int kilkist;
+    public String virobnyk = new String(new char[20]);
+}
+
+package <missing>;
+
+public class GlobalMembers
+{
+    public static int n = 5;
+    public static void main(String[] args)
+    {
+
+
+        tovar[n] = {"Сосиски", 50, 20.10, 15, 130, "Завод Петровский", "Хлеб", 14, 19.10, 5, 150, "Хлебозавод", "Вафли", 25, 19.10, 30, 98, "Вафлезавод", "Растишка", 40, 21.10, 45, 73, "Молокозавод", "Баунти", 35, 20.10, 35, 31, "Завод кондитерский"};
+        System.out.print("\t***** Spisok tovarov *****\n");
+    }
+}
